335b83f1f97d0f16c8996985b2e4ff691225e3e0
salman <salmann9977@gmail.com>
2023-07-09 23:23:35 +0530

    create store modal provider integration using zustand

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useEffect } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function SetupPage() {
+
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  },[isOpen, onOpen]);
+
     return (
       <div className="p-4">
-        <Button variant="destructive">Protected route</Button>
-        <UserButton afterSignOutUrl="/"/>
+        Root Page
       </div>
     )
   }
