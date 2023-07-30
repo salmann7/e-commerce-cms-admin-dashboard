35f4db0795580d8af3f03c5eacb070524b75b382
salman <salmann9977@gmail.com>
2023-07-30 15:15:42 +0530

    cloudinary api integration for img upload

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="kag3ot3g">
         {({ open }) => {
           const onClick = () => {
             open();
